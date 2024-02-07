8839eb26e2b13b896d738a1cc2478998b28914c5
Vivek works <159325026+vivekbworks@users.noreply.github.com>
2024-02-08 00:21:57 +0530

    add behave automation code
    first feature file

---
+++ Features/environment.py
@@ -0,0 +1,9 @@
+# features/environment.py
+# from selenium import webdriver
+#
+#
+# def before_feature(context, feature):
+#     context.driver = webdriver.Chrome()
+#
+# def after_feature(context, feature):
+#     context.driver.quit()
